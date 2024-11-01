0113ca7ca0e0e4c377f2e29a8e4f81c9e7616a84
Jona <jona07072000@gmail.com>
2024-11-01 01:30:18 -0500

    feat(components): add BadgePrimary, icons, and Container components refactor(utils): improve badge components and add dynamic icon support feat(pages): complete initial version of Inicio component

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        //TODO
+        // primary: '#FF6363',
+        secondary: '#004aad',
+        available: '#149603',
+
+      },
+      //TODO
+      // fontFamily: {
+      //   // body: ['Nunito'],
+      // },
+    },
+  },
+  plugins: [],
+}
