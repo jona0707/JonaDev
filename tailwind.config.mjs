ad467e664ecc0d1becf44a9ff4a0a40893b6f90a
Jona <jona07072000@gmail.com>
2024-11-04 16:40:58 -0500

    feat(about-me): add the new section with some text about me.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -16,5 +16,8 @@ export default {
       // },
     },
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [
+    require('flowbite/plugin'),
+    require('@tailwindcss/typography'),
+  ],
 }
